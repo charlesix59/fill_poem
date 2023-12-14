9ad412e70ac21f6365d7136b385458855a1bbe3b
charles-min <1974429496@qq.com>
2023-12-14 13:37:36 +0800

    fix: test

--- App.tsx
+++ App.tsx
@@ -77,8 +77,7 @@ function App(): React.JSX.Element {
             backgroundColor: isDarkMode ? Colors.black : Colors.white,
           }}>
           <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
+            Edit <Text style={styles.highlight}>App.tsx</Text> test test
           </Section>
           <Section title="See Your Changes">
             <ReloadInstructions />
