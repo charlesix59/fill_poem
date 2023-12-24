56d3959fc05ce505f2c4cfd76f4268f8546962e2
charles-min <1974429496@qq.com>
2023-12-24 11:11:00 +0800

    fix：添加toast

--- src/views/word/menu.tsx
+++ src/views/word/menu.tsx
@@ -1,24 +1,35 @@
 import React from "react";
 import Container from "../../components/container";
 import ButtonItem from "../../components/buttonItem";
+import {Provider, Toast} from "@ant-design/react-native";
 
 function WordMenu({navigation}: any): React.JSX.Element {
   return (
-    <Container>
-      <ButtonItem
-        onPress={() => {
-          navigation.navigate("WordCatalog", {type: "shi"});
-        }}>
-        平水韵
-      </ButtonItem>
-      <ButtonItem
-        onPress={() => {
-          navigation.navigate("WordCatalog", {type: "ci"});
-        }}>
-        词林正韵
-      </ButtonItem>
-      <ButtonItem>中华新韵</ButtonItem>
-    </Container>
+    <Provider>
+      <Container>
+        <ButtonItem
+          onPress={() => {
+            navigation.navigate("WordCatalog", {type: "shi"});
+          }}>
+          平水韵
+        </ButtonItem>
+        <ButtonItem
+          onPress={() => {
+            navigation.navigate("WordCatalog", {type: "ci"});
+          }}>
+          词林正韵
+        </ButtonItem>
+        <ButtonItem
+          onPress={() => {
+            Toast.info({
+              content: "中华新韵支持开发中，请等待更新",
+              duration: 1,
+            });
+          }}>
+          中华新韵
+        </ButtonItem>
+      </Container>
+    </Provider>
   );
 }
 
